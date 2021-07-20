9646452b1e889d690e536417d14fcacad9a63ebe
Nicolas Lecoy <nicolas.lecoy@gmail.com>
2021-07-20 08:38:32 +0200

    Add all types

--- notion/types.py
+++ notion/types.py
@@ -66,6 +66,42 @@ class RollupFunction(str, Enum):
     SHOW_ORIGINAL = "show_original"
 
 
+class PaginatedList(GenericModel, Generic[APISingularObject]):
+    object: str = "list"
+    results: List[APISingularObject]
+    has_more: bool
+    next_cursor: Optional[str] = None
+
+
+class UserType(str, Enum):
+    BOT = "bot"
+    PERSON = "person"
+
+
+class UserBase(BaseModel):
+    object: str = Field("user", const=True)
+    id: str
+    type: Optional[str]
+    name: str
+    avatar_url: Optional[str]
+
+
+class BotUser(UserBase):
+    type: UserType = Field(UserType.BOT, const=True)
+
+
+class Person(BaseModel):
+    email: EmailStr
+
+
+class PersonUser(UserBase):
+    type: UserType = Field(UserType.PERSON, const=True)
+    person: Optional[Person]
+
+
+User = TypeVar("User", BotUser, PersonUser)
+
+
 class Annotations(BaseModel):
     bold: bool
     italic: bool
@@ -80,6 +116,67 @@ class RichTextBaseInput(BaseModel):
     href: Optional[HttpUrl]
     annotations: Optional[Annotations]
     type: str
+
+
+class Text(BaseModel):
+    content: str
+    link: Optional[HttpUrl]
+
+
+class RichTextTextInput(RichTextBaseInput):
+    type: str = Field("text", const=True)
+    text: Text
+
+
+class RichTextBase(RichTextBaseInput):
+    plain_text: str
+    annotations: Annotations
+
+
+class RichTextText(RichTextTextInput):
+    plain_text: str
+    annotations: Annotations
+
+
+class Mention(BaseModel):
+    id: str
+
+
+class DatabaseMention(BaseModel):
+    type: str = Field("database", const=True)
+    database: Mention
+
+
+class DateMention(BaseModel):
+    type: str = Field("date", const=True)
+
+
+class PageMention(BaseModel):
+    type: str = Field("page", const=True)
+    page: Mention
+
+
+class UserMention(BaseModel):
+    type: str = Field("user", const=True)
+    user: User
+
+
+class RichTextMention(RichTextBase):
+    type: str = Field("mention", const=True)
+    mention: Union[UserMention, PageMention, DatabaseMention, DateMention]
+
+
+class Equation(BaseModel):
+    expression: str
+
+
+class RichTextEquation(RichTextBase):
+    type: str = Field("equation", const=True)
+    equation: Equation
+
+
+RichText = TypeVar("RichText", RichTextText, RichTextMention, RichTextEquation)
+RichTextInput = TypeVar("RichTextInput", RichTextTextInput, RichTextMention, RichTextEquation)
 
 
 class PropertyBase(BaseModel):
@@ -261,6 +358,7 @@ class BlockBase(BaseModel):
 
 
 class Paragraph(BaseModel):
+    text: List[RichText]
     children: Optional[List["Block"]]
 
 
@@ -269,26 +367,95 @@ class ParagraphBlock(BlockBase):
     paragraph: Paragraph
 
 
+class Heading(BaseModel):
+    text: List[RichText]
+
+
 class HeadingOneBlock(BlockBase):
     type: str = Field("heading_1", const=True)
+    heading_1: Heading
     has_children: bool = Field(False, const=True)
 
 
 class HeadingTwoBlock(BlockBase):
     type: str = Field("heading_2", const=True)
+    heading_2: Heading
     has_children: bool = Field(False, const=True)
 
 
 class HeadingThreeBlock(BlockBase):
     type: str = Field("heading_3", const=True)
+    heading_3: Heading
     has_children: bool = Field(False, const=True)
 
 
-Block = TypeVar("Block", ParagraphBlock, HeadingOneBlock, HeadingTwoBlock, HeadingThreeBlock)
+class BulletedListItem(BaseModel):
+    text: List[RichText]
+    children: Optional[List["Block"]]
 
 
-# Database Parent
-# Reference: https://developers.notion.com/reference/page#database-parent
+class BulletedListItemBlock(BlockBase):
+    type: str = Field("bulleted_list_item", const=True)
+    bulleted_list_item: BulletedListItem
+
+
+class NumberedListItem(BaseModel):
+    text: List[RichText]
+    children: Optional[List["Block"]]
+
+
+class NumberedListItemBlock(BlockBase):
+    type: str = Field("numbered_list_item", const=True)
+    numbered_list_item: NumberedListItem
+
+
+class Todo(BaseModel):
+    text: List[RichText]
+    checked: bool
+    children: Optional[List["Block"]]
+
+
+class ToDoBlock(BlockBase):
+    type: str = Field("to_do", const=True)
+    to_do: Todo
+
+
+class Toggle(BaseModel):
+    text: List[RichText]
+    children: Optional[List["Block"]]
+
+
+class ToggleBlock(BlockBase):
+    type: str = Field("toggle", const=True)
+    toggle: Toggle
+
+
+class ChildPage(BaseModel):
+    title: str
+
+
+class ChildPageBlock(BlockBase):
+    type: str = Field("child_page", const=True)
+    child_page: ChildPage
+
+
+class UnsupportedBlock(BlockBase):
+    type: str = Field("unsupported", const=True)
+
+
+Block = TypeVar(
+    "Block",
+    ParagraphBlock,
+    HeadingOneBlock,
+    HeadingTwoBlock,
+    HeadingThreeBlock,
+    BulletedListItemBlock,
+    NumberedListItemBlock,
+    ToDoBlock,
+    ToggleBlock,
+    ChildPageBlock,
+    UnsupportedBlock,
+)
 
 
 class ParentDatabase(BaseModel):
@@ -296,17 +463,9 @@ class ParentDatabase(BaseModel):
     database_id: str
 
 
-# Page Parent
-# Reference: https://developers.notion.com/reference/page#page-parent
-
-
 class ParentPage(BaseModel):
     type: str = Field("page_id", const=True)
     page_id: str
-
-
-# Workspace Parent
-# https://developers.notion.com/reference/page#workspace-parent
 
 
 class ParentWorkspace(BaseModel):
@@ -315,38 +474,16 @@ class ParentWorkspace(BaseModel):
 
 
 class Database(BaseModel):
-    """
-    Database object
-
-    Database objects describe the property schema of a database in Notion.
-    Pages are the items (or children) in a database.
-    Page property values must conform to the property objects laid out in the parent database object.
-
-    Reference: https://developers.notion.com/reference/database
-    """
-
     object: str = Field("database", const=True)
     id: str
     parent: Union[ParentPage, ParentWorkspace]
     created_time: datetime
     last_edited_time: datetime
+    title: List[RichText]
     properties: Dict[str, Property]
 
 
 class Page(BaseModel):
-    """
-    Page object
-
-    The Page object contains the property values of a single Notion page.
-    All pages have a parent. If the parent is a database,
-    the property values conform to the schema laid out database's properties.
-    Otherwise, the only property value is the title.
-    Page content is available as blocks.
-    The content can be read using retrieve block children and appended using append block children.
-
-    Reference: https://developers.notion.com/reference/page
-    """
-
     object: str = Field("page", const=True)
     id: str
     parent: Union[ParentDatabase, ParentPage, ParentWorkspace]
@@ -356,50 +493,305 @@ class Page(BaseModel):
     url: HttpUrl
 
 
-class UserType(str, Enum):
-    BOT = "bot"
-    PERSON = "person"
-
-
-class UserBase(BaseModel):
-    object: str = Field("user", const=True)
+class PropertyValueBase(BaseModel):
     id: str
-    type: Optional[str]
+    type: str
+
+
+class TitlePropertyValue(PropertyValueBase):
+    type: str = Field("title", const=True)
+    title: List[RichText]
+
+
+class RichTextPropertyValue(PropertyValueBase):
+    type: str = Field("rich_text", const=True)
+    rich_text: List[RichText]
+
+
+class TitleInputPropertyValue(PropertyValueBase):
+    type: str = Field("title", const=True)
+    title: List["RichTextInput"]
+
+
+class RichTextInputPropertyValue(PropertyValueBase):
+    type: str = Field("rich_text", const=True)
+    rich_text: List["RichTextInput"]
+
+
+class NumberPropertyValue(PropertyValueBase):
+    type: str = Field("number", const=True)
+    number: Union[int, float]
+
+
+class SelectPropertyValue(PropertyValueBase):
+    type: str = Field("select", const=True)
+    select: SelectOption
+
+
+class MultiSelectPropertyValue(PropertyValueBase):
+    type: str = Field("multi_select", const=True)
+    multi_select: List[MultiSelectOption]
+
+
+class StartEndDate(BaseModel):
+    start: datetime
+    end: Optional[datetime]
+
+
+class DatePropertyValue(PropertyValueBase):
+    type: str = Field("date", const=True)
+    date: StartEndDate
+
+
+class StringFormulaValue(BaseModel):
+    type: str = Field("str", const=True)
+    str: Optional[str]
+
+
+class NumberFormulaValue(BaseModel):
+    type: str = Field("number", const=True)
+    number: Optional[Union[int, float]]
+
+
+class BooleanFormulaValue(BaseModel):
+    type: str = Field("boolean", const=True)
+    boolean: bool
+
+
+class DateFormulaValue(BaseModel):
+    type: str = Field("date", const=True)
+    date: DatePropertyValue
+
+
+class FormulaPropertyValue(PropertyValueBase):
+    type: str = Field("formula", const=True)
+    formula: Union[StringFormulaValue, NumberFormulaValue, BooleanFormulaValue, DateFormulaValue]
+
+
+class NumberRollupValue(BaseModel):
+    type: str = Field("number", const=True)
+    number: Union[int, float]
+
+
+class DateRollupValue(BaseModel):
+    type: str = Field("date", const=True)
+    date: DatePropertyValue
+
+
+class ArrayRollupValue(BaseModel):
+    type: str = Field("array", const=True)
+    array: List["PropertyValueWithoutId"]
+
+
+class RollupPropertyValue(PropertyValueBase):
+    type: str = Field("rollup", const=True)
+    rollup: Union[NumberRollupValue, DateRollupValue, ArrayRollupValue]
+
+
+class PeoplePropertyValue(PropertyValueBase):
+    type: str = Field("people", const=True)
+    people: List[User]
+
+
+class FileName(BaseModel):
     name: str
-    avatar_url: Optional[str]
 
 
-class BotUser(UserBase):
-    type: UserType = Field(UserType.BOT, const=True)
+class FilesPropertyValue(PropertyValueBase):
+    type: str = Field("files", const=True)
+    files: List[FileName]
 
 
-class Person(BaseModel):
-    email: EmailStr
+class CheckboxPropertyValue(PropertyValueBase):
+    type: str = Field("checkbox", const=True)
+    checkbox: bool
 
 
-class PersonUser(UserBase):
-    type: UserType = Field(UserType.PERSON, const=True)
-    person: Optional[Person]
+class URLPropertyValue(PropertyValueBase):
+    type: str = Field("url", const=True)
+    url: str
 
 
-User = TypeVar("User", BotUser, PersonUser)
+class EmailPropertyValue(PropertyValueBase):
+    type: str = Field("email", const=True)
+    email: str
 
 
-class PaginatedList(GenericModel, Generic[APISingularObject]):
-    """
-    Pagination object
+class PhoneNumberPropertyValue(PropertyValueBase):
+    type: str = Field("phone_number", const=True)
+    phone_number: str
 
-    Endpoints which return a list of objects use pagination.
-    Pagination allows an integration to request a part of the list,
-    receiving an array of results and a next_cursor in the response.
-    The integration can use the next_cursor in another request to receive the next part of the list.
-    Using this technique, the integration can continue to make requests
-    to receive the whole list (or just the parts the integration needs).
 
-    Reference: https://developers.notion.com/reference/pagination
-    """
+class CreatedTimePropertyValue(PropertyValueBase):
+    type: str = Field("created_time", const=True)
+    created_time: datetime
 
-    object: str = "list"
-    results: List[APISingularObject]
-    has_more: bool
-    next_cursor: Optional[str] = None
+
+class CreatedByPropertyValue(PropertyValueBase):
+    type: str = Field("created_by", const=True)
+    created_by: User
+
+
+class LastEditedTimePropertyValue(PropertyValueBase):
+    type: str = Field("last_edited_time", const=True)
+    last_edited_time: datetime
+
+
+class LastEditedByPropertyValue(PropertyValueBase):
+    type: str = Field("last_edited_by", const=True)
+    last_edited_by: User
+
+
+PropertyValue = TypeVar(
+    "PropertyValue",
+    TitlePropertyValue,
+    RichTextPropertyValue,
+    NumberPropertyValue,
+    SelectPropertyValue,
+    MultiSelectPropertyValue,
+    DatePropertyValue,
+    FormulaPropertyValue,
+    RollupPropertyValue,
+    PeoplePropertyValue,
+    FilesPropertyValue,
+    CheckboxPropertyValue,
+    URLPropertyValue,
+    EmailPropertyValue,
+    PhoneNumberPropertyValue,
+    CreatedTimePropertyValue,
+    CreatedByPropertyValue,
+    LastEditedTimePropertyValue,
+    LastEditedByPropertyValue,
+)
+
+InputPropertyValueWithRequiredId = TypeVar(
+    "InputPropertyValueWithRequiredId",
+    TitleInputPropertyValue,
+    RichTextInputPropertyValue,
+    NumberPropertyValue,
+    SelectPropertyValue,
+    MultiSelectPropertyValue,
+    DatePropertyValue,
+    FormulaPropertyValue,
+    RollupPropertyValue,
+    PeoplePropertyValue,
+    FilesPropertyValue,
+    CheckboxPropertyValue,
+    URLPropertyValue,
+    EmailPropertyValue,
+    PhoneNumberPropertyValue,
+    CreatedTimePropertyValue,
+    CreatedByPropertyValue,
+    LastEditedTimePropertyValue,
+    LastEditedByPropertyValue,
+)
+
+
+class PropertyValueWithoutId(Generic[PropertyValue]):
+    class Config:
+        fields = {"id": {"exclude": True}}
+
+
+class TitlePropertySchema(BaseModel):
+    title: Dict = Field({}, const=True)
+
+
+class RichTextPropertySchema(BaseModel):
+    rich_text: Dict = Field({}, const=True)
+
+
+class NumberOptionalFormat(BaseModel):
+    format: Optional[NumberFormat]
+
+
+class NumberPropertySchema(BaseModel):
+    number: NumberOptionalFormat
+
+
+class SelectOptionSchema(BaseModel):
+    name: str
+    color: Optional[Color]
+
+
+MultiSelectOptionSchema = SelectOptionSchema
+
+
+class SelectOptional(BaseModel):
+    options: Optional[List[SelectOption]]
+
+
+class MultiSelectOptional(BaseModel):
+    options: Optional[List[MultiSelectOption]]
+
+
+class SelectPropertySchema(BaseModel):
+    select: SelectOptional
+
+
+class MultiSelectPropertySchema(BaseModel):
+    multi_select: MultiSelectOptional
+
+
+class DatePropertySchema(BaseModel):
+    date: Dict = Field({}, const=True)
+
+
+class PeoplePropertySchema(BaseModel):
+    people: Dict = Field({}, const=True)
+
+
+class FilePropertySchema(BaseModel):
+    files: Dict = Field({}, const=True)
+
+
+class CheckboxPropertySchema(BaseModel):
+    checkbox: Dict = Field({}, const=True)
+
+
+class URLPropertySchema(BaseModel):
+    url: Dict = Field({}, const=True)
+
+
+class EmailPropertySchema(BaseModel):
+    email: Dict = Field({}, const=True)
+
+
+class PhoneNumberPropertySchema(BaseModel):
+    phone_number: Dict = Field({}, const=True)
+
+
+class CreatedTimePropertySchema(BaseModel):
+    created_time: Dict = Field({}, const=True)
+
+
+class CreatedByPropertySchema(BaseModel):
+    created_by: Dict = Field({}, const=True)
+
+
+class LastEditedTimePropertySchema(BaseModel):
+    last_edited_time: Dict = Field({}, const=True)
+
+
+class LastEditedByPropertySchema(BaseModel):
+    last_edited_by: Dict = Field({}, const=True)
+
+
+PropertySchema = TypeVar(
+    "PropertySchema",
+    TitlePropertySchema,
+    RichTextPropertySchema,
+    NumberPropertySchema,
+    SelectPropertySchema,
+    MultiSelectPropertySchema,
+    DatePropertySchema,
+    PeoplePropertySchema,
+    FilePropertySchema,
+    CheckboxPropertySchema,
+    URLPropertySchema,
+    EmailPropertySchema,
+    PhoneNumberPropertySchema,
+    CreatedTimePropertySchema,
+    CreatedByPropertySchema,
+    LastEditedTimePropertySchema,
+    LastEditedByPropertySchema,
+)
